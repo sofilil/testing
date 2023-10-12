a07e99d6a8f0979a416ace74d6da9bc42e62387b
sofilil <97602288+sofilil@users.noreply.github.com>
2023-10-12 17:21:38 +0200

    third commit

--- questions.test.js
+++ questions.test.js
@@ -1,9 +1,31 @@
-import questions from './questions.json';
+const fs = require('fs');
+const questions = require('./questions.json');
 
 
 describe('kjører tester på questions', () => {
     test('correct : true', () => {
         expect("correct").toBeTruthy();
-})
+});
+
+    test('skal ha et riktig svar', () => {
+    questions.questions.forEach((question) => {
+      const correctAnswers = question.answers.filter((answer) => answer.correct);
+      expect(correctAnswers.length).toBe(1);
+    });
+});
+
+    test('skal ha title', () => {
+    questions.questions.forEach((question) => {
+      expect(question.title.trim()).not.toBe("");
+    });
+});
+
+    test('skal ha hjelpetekst', () => {
+    questions.questions.forEach((question) => {
+      if (question.helper) {
+        expect(question.helper.text.trim()).not.toBe("");
+      }
+    });
+ });
 
 })
