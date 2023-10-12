852f58847ffa86ec3927cbedbb1777b1beecd156
sofilil <97602288+sofilil@users.noreply.github.com>
2023-10-12 17:11:25 +0200

    second commit

--- questions.test.js
+++ questions.test.js
@@ -1,9 +1,9 @@
 import questions from './questions.json';
 
+
 describe('kjører tester på questions', () => {
     test('correct : true', () => {
         expect("correct").toBeTruthy();
 })
 
-    test()
 })
